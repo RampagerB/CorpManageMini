1a77229cf0e9e3a1e1164638648134eb21240295
MoonGravity <raybao27@gmail.com>
2014-08-20 16:03:31 +0800

    0.001v

--- myweb.py
+++ myweb.py
@@ -15,7 +15,8 @@ class Application(tornado.web.Application):
 	def __init__(self):
 		handlers = [(r"/", MainHandler),
 					(r"/regist", RegistHandler),
-					(r"/login", LoginHandler)]
+					(r"/login", LoginHandler),
+					(r"/exit", LogoutHandler)]
 		settings = dict(
             template_path = os.path.join(os.path.dirname(__file__), "templates"),
             static_path = os.path.join(os.path.dirname(__file__), "static"),
@@ -29,6 +30,7 @@ class Application(tornado.web.Application):
 		tornado.web.Application.__init__(self, handlers, **settings)
 
 class MainHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
 	def get(self):
 		staff_cookie = self.get_secure_cookie('staff')
 		basic = self.application.db.basic
@@ -38,21 +40,31 @@ class MainHandler(tornado.web.RequestHandler):
 		self.render("index.html",basic = info,staffs = staffs,user = staff_cookie)
 
 class LoginHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
 	def post(self):
 		staffs = self.application.db.staff
 		pw = self.get_argument('user_pw')
-		staff = staffs.find_one({'user_id':self.get_argument['user_id'],'user_pw':hashlib.new('md5',pw).hexdigest()})
+		remote_ip = self.request.remote_ip
+		staff = staffs.find_one({'user_id':self.get_argument('user_id'),'user_pw':hashlib.new('md5',pw).hexdigest(),\
+			'user_ip':remote_ip})
 		if staff:
-			if self.get_argument['ifCookie']=='1':
-				self.set_secure_cookie('staff',staff)
-				self.redirect('/')
+			self.set_secure_cookie('staff',staff)
+			self.render('/')
 		else:
-			self.write("登录失败！")
+			self.write("编号或密码输入错误/IP地址不符。")
+
+class LogoutHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
+	def get(self):
+		self.clear_cookie("user")
+		self.redirect("/")
 
 class RegistHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
 	def get(self):
 		self.render("regist.html")
 
+	@tornado.web.asynchronous
 	def post(self):
 		db = self.application.db.staff
 		db.insert({'user_id':self.get_argument('user_id'),'user_name':self.get_argument('user_name'),\
@@ -63,8 +75,6 @@ class LoginModule(tornado.web.UIModule):
 	def render(self,staff):
 		return self.render_string('modules/login.html',staff = staff)
 
-
-	
 
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
