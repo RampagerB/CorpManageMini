5172e0f01cf0f319d2bc171f5da3aaf9fab14d18
MoonGravity <raybao27@gmail.com>
2014-09-11 16:05:51 +0800

    Login and Users add

--- myweb.py
+++ myweb.py
@@ -8,6 +8,8 @@ import hashlib
 
 import pymongo
 
+from bson.json_util import dumps,loads
+
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
@@ -16,6 +18,7 @@ class Application(tornado.web.Application):
 		handlers = [(r"/", MainHandler),
 					(r"/regist", RegistHandler),
 					(r"/login", LoginHandler),
+					(r"/changepw/(\w*)", ChangePwHandler),
 					(r"/exit", LogoutHandler)]
 		settings = dict(
             template_path = os.path.join(os.path.dirname(__file__), "templates"),
@@ -33,30 +36,31 @@ class MainHandler(tornado.web.RequestHandler):
 	@tornado.web.asynchronous
 	def get(self):
 		staff_cookie = self.get_secure_cookie('staff')
-		basic = self.application.db.basic
-		info = basic.find_one()
 		staff = self.application.db.staff
 		staffs = staff.find()
-		self.render("index.html",basic = info,staffs = staffs,user = staff_cookie)
+		self.render("index.html",staffs = staffs,user = staff_cookie)
 
 class LoginHandler(tornado.web.RequestHandler):
 	@tornado.web.asynchronous
 	def post(self):
 		staffs = self.application.db.staff
+		users = staffs.find()
+		uid = self.get_argument('user_id')
 		pw = self.get_argument('user_pw')
-		remote_ip = self.request.remote_ip
-		staff = staffs.find_one({'user_id':self.get_argument('user_id'),'user_pw':hashlib.new('md5',pw).hexdigest(),\
-			'user_ip':remote_ip})
+		staff = staffs.find_one({'user_id':uid,'user_pw':hashlib.new('md5',pw).hexdigest()})
 		if staff:
+			staff = dumps(staff)
 			self.set_secure_cookie('staff',staff)
-			self.render('/')
+			self.render('index.html',user=staff,staffs=users)
+			self.finish()
 		else:
-			self.write("编号或密码输入错误/IP地址不符。")
+			self.write("编号或密码输入错误")
+			self.finish()
 
 class LogoutHandler(tornado.web.RequestHandler):
 	@tornado.web.asynchronous
 	def get(self):
-		self.clear_cookie("user")
+		self.clear_cookie("staff")
 		self.redirect("/")
 
 class RegistHandler(tornado.web.RequestHandler):
@@ -73,8 +77,22 @@ class RegistHandler(tornado.web.RequestHandler):
 
 class LoginModule(tornado.web.UIModule):
 	def render(self,staff):
-		return self.render_string('modules/login.html',staff = staff)
+		if staff:
+			staff = loads(staff)
+		return self.render_string('modules/login.html',user = staff)
 
+class ChangePwHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
+	def get(self,user_id):
+		self.render("changepw.html",user_id=user_id)
+
+	@tornado.web.asynchronous
+	def post(self):
+		user = self.get_secure_cookie("staff")
+		db = self.application.db.staff
+		db.insert({'user_id':self.get_argument('user_id'),'user_name':self.get_argument('user_name'),\
+			'user_ip':self.get_argument('user_ip'),'user_pw':hashlib.new('md5',self.get_argument('user_pw')).hexdigest()})
+		self.redirect('/')
 
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
